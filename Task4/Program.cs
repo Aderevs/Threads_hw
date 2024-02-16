7d15317340723b3278c4520c05b5c6f95c6b2eda
Yura <aderevs@gmail.com>
2024-02-16 20:08:15 +0200

    task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+
+        static int counter = 0;
+
+        static object block = new object(); // block - не повинен бути структурним.
+
+        static void Function()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                // Встановлюється блокування кожен (50!) разів у новий object (boxing).
+                Monitor.Enter(block); // boxing створює новий об'єкт (50 об'єктів).
+
+                // Виконання деякої роботи потоком ...
+                Console.WriteLine(++counter);
+
+                // Спроба зняти блокування з об'єкта, який не є об'єктом блокування.
+                Monitor.Exit(block); // boxing створює абсолютно новий об'єкт.
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads)
+                thread.Start();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
